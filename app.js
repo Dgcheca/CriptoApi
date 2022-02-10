25d815d8a1d32f6a348ce8c0f08c2bdd2f84a119
Daniel Gomez Checa <mardukecheka@gmail.com>
2022-02-10 09:41:02 +0100

    1002

--- app.js
+++ app.js
@@ -2,14 +2,13 @@ const express = require("express");
 const app = express();
 
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 
-//Cargar rutas
+// Cargar rutas
+const hello_routes = require("./routes/hello");
 const task_routes = require("./routes/task");
-const user_routes = require("./routes/user");
 
-//rutas base
+// Rutas base
 app.use("/api", task_routes);
-app.use("/api", user_routes);
 
 module.exports = app;
--- index.js
+++ index.js
@@ -1,30 +1,31 @@
 const mongoose = require("mongoose");
-const app = require("./CriptoApi");
+const app = require("./app");
 const port = process.env.PORT || 3977;
-const urlMongoDb = "mongodb+srv://DGCheca:jaroso@mdbatlas.it8sj.mongodb.net/ApiRest?retryWrites=true&w=majority";
+const urlMongoDb =
+"mongodb+srv://DGCheca:jaroso@mdbatlas.it8sj.mongodb.net/ApiRest?retryWrites=true&w=majority";
 
 mongoose.connect(
-    urlMongoDb,
-    { 
-        useNewUrlParser:true, 
-        useUnifiedTopology: true, 
-        useFindAndModifiy: false,
-    }, 
-    (err,res) =>{
-        try {
-            if (err) {
-                throw err;
-            } else {
-                console.log("conexion a la base de datos correcta");
+  urlMongoDb,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+  },
+  (err, res) => {
+    try {
+      if (err) {
+        throw err;
+      } else {
+        console.log("La conexion a la base de datos es correcta");
 
-                app.listen(port, ()=> {
-                    console.log(
-                        "A tope con el servidor" + port
-                        );
-                });
-                
-            }
-        } catch (error) {
-            console.error(error);
-        }
-});
+        app.listen(port, () => {
+          console.log(
+            `Servidor del API REST esta funcionando en http://localhost:${port}`
+          );
+        });
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  }
+);
