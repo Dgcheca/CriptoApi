ec42fe1d4f7a34b2530d47a344fef183e7792d25
Daniel Gomez Checa <mardukecheka@gmail.com>
2022-02-10 09:27:32 +0100

    1002

--- index.js
+++ index.js
@@ -1,24 +1,30 @@
 const mongoose = require("mongoose");
 const app = require("./app");
-const port = 3000;
+const port = process.env.PORT || 3977;
 const urlMongoDb = "mongodb+srv://DGCheca:jaroso@mdbatlas.it8sj.mongodb.net/ApiRest?retryWrites=true&w=majority";
 
-mongoose.connect(urlMongoDb, (err,res) =>{
-    try {
-        if (err) {
-            throw err
-        } else {
-            console.log("conexion a la base de datos correcta");
+mongoose.connect(
+    urlMongoDb,
+    { 
+        useNewUrlParser:true, 
+        useUnifiedTopology: true, 
+        useFindAndModifiy: false,
+    }, 
+    (err,res) =>{
+        try {
+            if (err) {
+                throw err;
+            } else {
+                console.log("conexion a la base de datos correcta");
 
-            app.listen(port, ()=> {
-                console.log("A tope con el servidor");
-            });
-            
+                app.listen(port, ()=> {
+                    console.log(
+                        "A tope con el servidor" + port
+                        );
+                });
+                
+            }
+        } catch (error) {
+            console.error(error);
         }
-    } catch (error) {
-        console.error(error);
-    }
 });
-
-
-//mongodb+srv://DGCheca:jaroso@mdbatlas.it8sj.mongodb.net/ApiRest?retryWrites=true&w=majority
